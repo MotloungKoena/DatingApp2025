514f2329cab0ac66aa02dfb3a34afa8beb454184
Motloung Koena <2019476593@ufs4life.ac.za>
2025-04-12 22:31:59 +0200

    End of Section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,4 +7,30 @@ namespace API.Data;
 public class DataContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<UserLike> Likes {get;set;}
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Specify primary key
+        builder.Entity<UserLike>()
+            .HasKey(k => new { k.SourceUserId, k.TargetUserId });
+
+        // Set the relationships
+        builder.Entity<UserLike>()
+            .HasOne(s => s.SourceUser)
+            .WithMany(l => l.LikedUsers)
+            .HasForeignKey(s => s.SourceUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        builder.Entity<UserLike>()
+            .HasOne(s => s.TargetUser)
+            .WithMany(l => l.LikedByUsers)
+            .HasForeignKey(s => s.TargetUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+        // Note: For SQL Server, set the DeleteBehavior to
+        // DeleteBehavior.NoAction or you will get an error during migration.
+    }
 }
--- API/Data/Migrations/20250402140543__UpdatedUserEntity.cs
+++ API/Data/Migrations/20250402140543__UpdatedUserEntity.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace API.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class UpdatedUserEntity : Migration
+    public partial class _UpdatedUserEntity : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
